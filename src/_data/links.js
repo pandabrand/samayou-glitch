2bf45e578eacb1592bdbe9ed4f066a4fd74119ec
Glitch (classy-iodized-frill) <none>
2023-04-07 03:14:04 +0000

    🖇️🤗 Updated with Glitch

---
+++ src/_data/links.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    name: "Bandcamp",
+    url: "https://samayou.bandcamp.com",
+    description: "Winter One EP available for download and limited edition cassette"
+  },
+  {
+    name: "Youtube",
+    url: "https://youtu.be/y_aaqbaA40k",
+    description: "Video for Small Lecture"
+  },
+  {
+    name: "Apple Music",
+    url: "https://music.apple.com/us/album/winter-one-single/1677868389"
+  },
+  {
+    name: "Spotify",
+    url: "https://spotify.link/W8FfX2RWvyb"
+  },
+  {
+    name: "Youtube Music",
+    url: "https://music.youtube.com/watch?v=UiZPckkhNb0&feature=share"
+  }
+];
